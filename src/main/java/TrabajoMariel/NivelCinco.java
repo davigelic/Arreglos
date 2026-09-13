79669301b3fb43239f8f550c6982b96856db14e1
TamaraAcunaS <mariel.acuna@ucr.ac.cr>
2026-09-13 00:07:51 -0600

    nivel 4 y 5

--- src/main/java/TrabajoMariel/NivelCinco.java
+++ src/main/java/TrabajoMariel/NivelCinco.java
@@ -4,6 +4,8 @@
  */
 package TrabajoMariel;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author kaysc
@@ -13,10 +15,66 @@ public class NivelCinco {
     //Nivel 5, ejercicio 19
     public void ComparacionVecinos(){
         
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de números de la lista"));
+        
+        if (cantidad <= 0){
+            JOptionPane.showMessageDialog(null,"La cantidad debe ser mayor que 0.");
+        } else{
+            int[] numeros = new int[cantidad];
+            
+            for (int i = 0; i < numeros.length; i++){
+                numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+            }
+            boolean ordenado = true;
+            //Comparacion
+            for (int i = 0; i < numeros.length - 1; i++){
+
+                if (numeros[i] > numeros[i + 1]){
+                    ordenado = false;
+                    break;
+                }
+                //Resultado
+                if (ordenado){
+                    JOptionPane.showMessageDialog(null,"El arreglo está ordenado de forma ascendente");
+                } else {
+                    JOptionPane.showMessageDialog(null,"El arreglo no está ordenado de forma ascendente");
+                }
+            }//Fin for
+        }
     }
     
     //Nivel 5, ejercicio 20
     public void ComparacionSimetrica(){
         
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de elementos de la lista"));
+        
+        if (cantidad <= 0){
+            JOptionPane.showMessageDialog(null,"La cantidad debe ser mayor que 0");
+        } else{
+            int[] numeros = new int[cantidad];
+            
+            for (int i = 0; i < numeros.length; i++){
+                numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+            }
+            
+            boolean capicua = true;
+            //Comparacion
+            for (int i = 0; i < numeros.length / 2; i++){
+
+                int indiceDerecha = numeros.length - 1 - i;
+
+                if (numeros[i] != numeros[indiceDerecha]){
+                    capicua = false;
+                    break;
+                }
+            }
+            
+            //Resultado
+            if (capicua){
+                JOptionPane.showMessageDialog(null,"El arreglo es capicúa");
+            } else{
+                JOptionPane.showMessageDialog(null,"El arreglo no es capicúa");
+            }
+        }
     }
 }
--- src/main/java/TrabajoMariel/NivelCuatro.java
+++ src/main/java/TrabajoMariel/NivelCuatro.java
@@ -4,6 +4,8 @@
  */
 package TrabajoMariel;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author kaysc
@@ -12,11 +14,54 @@ public class NivelCuatro {
     
     //Nivel 4, ejercicio 15
     public void RotacionDerecha(){
+        int[] numeros = new int[10];
         
+        for (int i = 0; i < numeros.length; i++){
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+        }
+        
+        int ultimo = numeros[numeros.length - 1];
+        
+        for (int i = numeros.length - 1; i > 0; i--){
+            numeros[i] = numeros[i - 1];
+        }
+        
+        numeros[0] = ultimo;
+        String mensaje = "Arreglo rotado:\n";
+         
+        for (int i = 0; i < numeros.length; i++){
+            mensaje += numeros[i] + " ";
+        }
+        JOptionPane.showMessageDialog(null, mensaje);
     }
     
     //Nivel 4, ejercicio 16
     public void ValidacionIndices(){
         
+        int[] numeros = new int[10];
+        
+        for (int i = 0; i < numeros.length; i++){
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+        }
+        //Solicitando los indices
+        int indiceUno = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer índice"));
+        int indiceDos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer índice"));
+        
+        if (indiceUno >= 0 && indiceUno < numeros.length && indiceDos >= 0 && indiceDos < numeros.length){
+            
+            //Intercambio de esos valores
+            int indice= numeros[indiceUno];
+            numeros[indiceUno] = numeros[indiceDos];
+            numeros[indiceDos] = indice;
+            
+            String mensaje = "Arreglo después del intercambio:\n";
+
+            for (int i = 0; i < numeros.length; i++) {
+                mensaje += numeros[i]+" ";
+            }
+            JOptionPane.showMessageDialog(null, mensaje);
+        } else{
+            JOptionPane.showMessageDialog(null,"Error: uno o ambos índices no son válidos.");
+        } 
     }
 }
